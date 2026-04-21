76090146d6c51134d3e5089ae8ca01eefed91d9c
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-04-21 17:56:27 +0530

    TE-407 Added .env File

--- Class/Database.php
+++ Class/Database.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . './../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 class Database
 {
 
@@ -6,17 +11,12 @@ class Database
 
     public function connect()
     {
-        $config = require __DIR__ . "./../config/config.php";
-
-        $db = $config['db'];
-
-
         $this->conn = new mysqli(
-            $db['host'],
-            $db['username'],
-            $db['password'],
-            $db['dbname'],
-            $db['port']
+                $_ENV['host'],
+                $_ENV['username'],
+                $_ENV['password'],
+                $_ENV['dbname'],
+                $_ENV['port']
         );
 
         if ($this->conn->connect_error) {
