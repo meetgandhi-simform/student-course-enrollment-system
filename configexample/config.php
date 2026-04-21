614fc9386ad1a601977e5e0dd23c0bc59c571148
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-04-21 17:26:58 +0530

    TE-407/Add Configuration example file

---
+++ configexample/config.php
@@ -0,0 +1,14 @@
+<?php
+
+//Change Folder Name To config
+
+return [
+    'db' => [
+        'host' => 'YOUR HOST NAME',
+        'username' => 'YOUR USER NAME',
+        'password' => 'YOU PASSWORD',
+        'dbname' => 'YOU DATABASE NAME',
+        'port' => 'YOUR DATABASE PORT'
+    ]
+];
+?>
