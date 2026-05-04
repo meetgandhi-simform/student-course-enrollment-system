90c37c7a44ace0ffec926c29d615832073cbd2bc
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-05-04 13:38:10 +0530

    add/TE-T407 added final database

--- enrollment_db.sql
+++ enrollment_db.sql
@@ -41,23 +41,9 @@ CREATE TABLE `course_instructor` (
   KEY `fk_ci_course` (`course_id`),
   CONSTRAINT `fk_ci_course` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_ci_instructor` FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 57 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
---
--- Dumping data for table `course_instructor`
---
-
-LOCK TABLES `course_instructor` WRITE;
-/*!40000 ALTER TABLE `course_instructor` DISABLE KEYS */
-;
-INSERT INTO `course_instructor`
-VALUES (1, 14, 1),
-(2, 16, 2),
-(3, 78, 3);
-/*!40000 ALTER TABLE `course_instructor` ENABLE KEYS */
-;
-UNLOCK TABLES;
 --
 -- Table structure for table `courses`
 --
@@ -72,24 +58,30 @@ CREATE TABLE `courses` (
   `course_name` varchar(255) DEFAULT NULL,
   `duration_weeks` int DEFAULT NULL,
   `max_seats` int NOT NULL,
+  `avail_seats` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
 --
--- Dumping data for table `courses`
+-- Table structure for table `email_queue`
 --
 
-LOCK TABLES `courses` WRITE;
-/*!40000 ALTER TABLE `courses` DISABLE KEYS */
+DROP TABLE IF EXISTS `email_queue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-INSERT INTO `courses`
-VALUES (1, 'PHP', 5, 100),
-(2, 'Java Script', 5, 100),
-(3, 'JQuery', 6, 80);
-/*!40000 ALTER TABLE `courses` ENABLE KEYS */
+/*!50503 SET character_set_client = utf8mb4 */
 ;
-UNLOCK TABLES;
+CREATE TABLE `email_queue` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `status` enum('pending', 'sent', 'failed') DEFAULT 'pending',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Table structure for table `enrollments`
 --
@@ -110,19 +102,9 @@ CREATE TABLE `enrollments` (
   KEY `course_instructor_id` (`course_instructor_id`),
   CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_instructor_id`) REFERENCES `course_instructor` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 43 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
---
--- Dumping data for table `enrollments`
---
-
-LOCK TABLES `enrollments` WRITE;
-/*!40000 ALTER TABLE `enrollments` DISABLE KEYS */
-;
-/*!40000 ALTER TABLE `enrollments` ENABLE KEYS */
-;
-UNLOCK TABLES;
 --
 -- Table structure for table `users`
 --
@@ -143,180 +125,17 @@ CREATE TABLE `users` (
   `isActive` enum('Active', 'Inactive') DEFAULT 'Active',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE = InnoDB AUTO_INCREMENT = 79 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */
-;
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */
-;
-INSERT INTO `users`
+) ENGINE = InnoDB AUTO_INCREMENT = 154 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO users(name, email, password, phone, role)
 VALUES (
-    1,
-    'Rohan',
-    'rohan.rohit@example.com',
-    '$2y$10$eBZoHdBSjsrMnKEduczWru.ZJt27e4eXMYpCLsWqCn/et0tWzLXQK',
-    123456789,
-    'Student',
-    '2026-04-13 10:20:57',
-    'Active'
-  ),
-(
-    4,
-    'Abhi',
-    'abhi.andani@example.com',
-    '$2y$10$a0eoFHTpcNLzov3Xy76aAub05H/j.r8o8cJ3kiKTEwvmhy6vhlS4.',
-    1234567890,
-    'Student',
-    '2026-04-13 10:25:36',
-    'Active'
-  ),
-(
-    5,
     'admin',
     'admin@example.com',
-    '$2y$10$eBZoHdBSjsrMnKEduczWru.ZJt27e4eXMYpCLsWqCn/et0tWzLXQK',
-    6351398001,
-    'Admin',
-    '2026-04-13 10:54:42',
-    'Active'
-  ),
-(
-    6,
-    'Dhruv',
-    'dhruv.rana@example.com',
-    '$2y$10$DrvCsbhGINktakDOZRoJNeYx4XBg3/.p4ebM22A.ajGZWtZ0sZl46',
-    1230987456,
-    'Student',
-    '2026-04-13 11:50:01',
-    'Active'
-  ),
-(
-    8,
-    'Ankush',
-    'ankush.dubey@example.com',
-    '$2y$10$yNxzgM6gojyTMtJw.BjYy.3tFphvA45ua6UhShN3aBGSsG6LrFvDy',
-    1230984567,
-    'Student',
-    '2026-04-13 11:51:38',
-    'Active'
-  ),
-(
-    12,
-    'Alice',
-    'alice.bob@example.com',
-    '$2y$10$KJG.YyxuNX.mC.edGDitouNvjCCIYjele2FHfoKkoq.4s4CFsVRI6',
-    1230984576,
-    'Student',
-    '2026-04-14 08:48:02',
-    'Active'
-  ),
-(
-    14,
-    'Shivang',
-    'shivang.rathod@example.com',
-    '$2y$10$8vZSwf79k2SGhyM1I5k8Veb7jusHLAUBV9o7gpXmJUaZ1XPV0cpmi',
-    1230987456,
-    'Instructor',
-    '2026-04-14 09:16:00',
-    'Active'
-  ),
-(
-    16,
-    'Denish',
-    'denish.bhimani@example.com',
-    '$2y$10$zKFI4UDyPCVAezkF2i7HTuOzVFPdBrQIo0oy0yVHdiiBOd8T81H8G',
-    981237465,
-    'Instructor',
-    '2026-04-14 10:21:20',
-    'Active'
-  ),
-(
-    17,
-    'Bob',
-    'bob.alice@example.com',
-    '$2y$10$ilili9sq6ORvJc279Yezw..7l4zBmHj/JfkyDrQDZW4ofHhgnMUoe',
-    1230948576,
-    'Student',
-    '2026-04-14 12:40:56',
-    'Active'
-  ),
-(
-    23,
-    'Raj',
-    'raj.patel@example.com',
-    '$2y$10$y2v6nIiKQim9mdRPaPEQweQ4OVDukRb9tosBlec8MDo4XuoIOtsl2',
-    987615432,
-    'Student',
-    '2026-04-15 09:59:47',
-    'Active'
-  ),
-(
-    24,
-    'Raju',
-    'raju.patel@example.com',
-    '$2y$12$Qn1f0riwRnzIZKZaX4rUXeaw7Ti4a2nO2nXP8rgp.e8BuOgbw.9QS',
-    981237645,
-    'Student',
-    '2026-04-15 11:59:13',
-    'Active'
-  ),
-(
-    25,
-    'Zampa',
-    'adam.zampa@example.com',
-    '$2y$10$0zWw/9NhqBbd3g0XXrglWOMs35GKtoffVc6gdbsmqDHTe8..bLVRG',
-    987651234,
-    'Student',
-    '2026-04-15 12:19:30',
-    'Active'
-  ),
-(
-    26,
-    'Starc',
-    'maitchel.starc@example.com',
-    '$2y$10$1EV.WvFMEQdU0qqLsxdByOzajVdQkcalIlPG1s9sXkiP7zRQKJBLi',
-    981237645,
-    'Student',
-    '2026-04-15 12:33:34',
-    'Active'
-  ),
-(
-    27,
-    'Rohan Mehta',
-    'rohan.mehta@example.com',
-    '$2y$12$oa5epl/lFFORt9ah/r.r1.UAPWMmg6RmE30RR201hhG7/nQbEi6bq',
-    9876543210,
-    'Student',
-    '2026-04-16 07:08:27',
-    'Active'
-  ),
-(
-    28,
-    'Sam',
-    'sam@example.com',
-    '$2y$12$M3k.OEcmyVjhFLSaB03h1e7DDWmn95Tg4MJ4EhF0fLSmk8SOH8b5C',
-    9123456781,
-    'Admin',
-    '2026-04-16 08:29:32',
-    'Active'
-  ),
-(
-    78,
-    'Divyang',
-    'divyang@example.com',
-    '$2y$12$45ruH18q3hXsVUX3ELlI8OmCuyQsX9jhHmXnY1gN1cxMP5BRb1v5m',
-    6351398110,
-    'Instructor',
-    '2026-04-17 05:34:39',
-    'Active'
+    '123',
+    9870654321,
+    'Admin'
   );
-/*!40000 ALTER TABLE `users` ENABLE KEYS */
+/*!40101 SET character_set_client = @saved_cs_client */
 ;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
 ;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */
@@ -333,4 +152,4 @@ UNLOCK TABLES;
 ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
 ;
--- Dump completed on 2026-04-21 14:51:51
+-- Dump completed on 2026-05-04 13:36:44
